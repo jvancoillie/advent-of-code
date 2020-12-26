c5b680fb0f65d06579308f062c1c7f8560b27d5f
Vancoillie <jeremy.vancoillie@gmail.com>
2020-12-26 13:22:54 +0100

    improuve PuzzleResolver class template

--- src/Resources/Skeleton/PuzzleResolver.tpl.php
+++ src/Resources/Skeleton/PuzzleResolver.tpl.php
@@ -10,6 +10,25 @@ class PuzzleResolver extends AbstractPuzzleResolver
 {
     public function main(PuzzleInput $input, OutputInterface $output)
     {
+        $this->part1($input, $output);
+        $this->part2($input, $output);
+    }
 
+    public function part1(PuzzleInput $input, OutputInterface $output)
+    {
+        $ans = 0;
+        foreach (explode("\n", $input->getData()) as $line){
+
+        }
+        $output->writeln("<info>Part 1 : $ans</info>");
+    }
+
+    public function part2(PuzzleInput $input, OutputInterface $output)
+    {
+        $ans = 0;
+        foreach (explode("\n", $input->getData()) as $line){
+
+        }
+        $output->writeln("<info>Part 2 : $ans</info>");
     }
 }
