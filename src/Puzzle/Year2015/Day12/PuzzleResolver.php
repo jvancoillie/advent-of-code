9589d87caad00cc908d81d03679b894343429ad7
Vancoillie <jeremy.vancoillie@gmail.com>
2020-12-27 16:37:02 +0100

    Year 2015 Day 12

---
+++ src/Puzzle/Year2015/Day12/PuzzleResolver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Puzzle\Year2015\Day12;
+
+use App\Puzzle\AbstractPuzzleResolver;
+use App\Puzzle\PuzzleInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class PuzzleResolver
+ * @see https://adventofcode.com/2015/day/12
+ */
+class PuzzleResolver extends AbstractPuzzleResolver
+{
+    public function main(PuzzleInput $input, OutputInterface $output)
+    {
+        $this->part1($input, $output);
+        $this->part2($input, $output);
+    }
+
+    public function part1(PuzzleInput $input, OutputInterface $output)
+    {
+        $decoded = json_decode($input->getData());
+        $ans = $this->sumNumbers($decoded);
+
+        $output->writeln("<info>Part 1 : $ans</info>");
+    }
+
+    public function part2(PuzzleInput $input, OutputInterface $output)
+    {
+        $decoded = json_decode($input->getData());
+        $ans = $this->sumNumbers($decoded, 'red');
+
+        $output->writeln("<info>Part 2 : $ans</info>");
+    }
+
+    public function sumNumbers($array, $excluded = null)
+    {
+        $sum = 0;
+        foreach ($array as $key => $item) {
+            if (is_object($item)) {
+                $array = (array)$item;
+                if ($excluded === null) {
+                    $sum += $this->sumNumbers($array, $excluded);
+                } elseif (!in_array($excluded, $array)) {
+                    $sum += $this->sumNumbers($array, $excluded);
+                }
+            } elseif (is_array($item)) {
+                $sum += $this->sumNumbers($item, $excluded);
+            } elseif (is_numeric($item)) {
+                $sum += $item;
+            }
+        }
+
+        return $sum;
+    }
+}
