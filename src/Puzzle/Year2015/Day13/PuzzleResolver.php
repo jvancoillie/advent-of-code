d61c5e2bbab17d7feb3b586858390b248b867caa
Vancoillie <jeremy.vancoillie@gmail.com>
2020-12-27 19:17:22 +0100

    Year 2015 Day 13

---
+++ src/Puzzle/Year2015/Day13/PuzzleResolver.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Puzzle\Year2015\Day13;
+
+use App\Puzzle\AbstractPuzzleResolver;
+use App\Puzzle\PuzzleInput;
+use App\Utils\PathFinding\TSP;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class PuzzleResolver
+ * @see https://adventofcode.com/2015/day/13
+ *
+ * execute with increase memory limit :
+ *  php -d  memory_limit=2048M bin/console puzzle:resolve --year=2015 --day=13
+ *
+ */
+class PuzzleResolver extends AbstractPuzzleResolver
+{
+    /**
+     * @var TSP
+     */
+    private $tsp;
+
+    public function main(PuzzleInput $input, OutputInterface $output)
+    {
+        $this->createTSP($input);
+        $this->part1($output);
+        $this->part2($output);
+    }
+
+    public function part1(OutputInterface $output)
+    {
+        $ans = $this->tsp->getLongestDistance();
+        $path = $this->tsp->getLongestPath();
+        $graph = $this->tsp->getGraph();
+
+        $first = array_shift($path);
+        $last = array_pop($path);
+
+        $ans += $graph[$first][$last];
+
+        $output->writeln("<info>Part 1 : $ans</info>");
+    }
+
+    public function part2(OutputInterface $output)
+    {
+        $graph = $this->tsp->getGraph();
+
+        foreach ($graph as $person => $next) {
+            $this->tsp->add($person, 'me', 0);
+        }
+
+        $ans = $this->tsp->getLongestDistance();
+        $path = $this->tsp->getLongestPath();
+        $graph = $this->tsp->getGraph();
+
+        $first = array_shift($path);
+        $last = array_pop($path);
+
+        $ans += $graph[$first][$last];
+
+        $output->writeln("<info>Part 2 : $ans</info>");
+    }
+
+    public function parseLine($line)
+    {
+        $pattern = '/^(?<from>.*)\swould\s(?<type>.*)\s(?<bonus>\d+)\shappiness units by sitting next to\s(?<next>.*).$/';
+        preg_match($pattern, $line, $matches);
+
+        if (!$matches) {
+            throw new \Exception("parsing action error");
+        }
+
+        return [$matches['from'], $matches['type'], $matches['bonus'], $matches['next']];
+    }
+
+    public function createTSP(PuzzleInput $input)
+    {
+        $graph = [];
+
+        foreach (explode("\n", $input->getData()) as $line) {
+            [$from, $type, $bonus, $to] = $this->parseLine($line);
+            $bonus = ($type === 'gain') ? (int)$bonus : (int)-$bonus;
+
+            if (isset($graph[$from][$to])) {
+                $graph[$from][$to] += $bonus;
+                $graph[$to][$from] += $bonus;
+            } else {
+                $graph[$from][$to] = $bonus;
+                $graph[$to][$from] = $bonus;
+                $graph[$to][$to] = 0;
+                $graph[$from][$from] = 0;
+            }
+        }
+
+        $this->tsp = new TSP($graph);
+    }
+}
--- src/Utils/PathFinding/TSP.php
+++ src/Utils/PathFinding/TSP.php
@@ -10,7 +10,6 @@ namespace App\Utils\PathFinding;
  */
 class TSP
 {
-
     /**
      * @var array
      */
@@ -23,6 +22,20 @@ class TSP
      * @var null
      */
     private $shortestDistance = null;
+
+    /**
+     * @var array
+     */
+    private $longestPath = [];
+    /**
+     * @var array
+     */
+    private $longestPaths = [];
+    /**
+     * @var null
+     */
+    private $longestDistance = null;
+
     /**
      * @var array
      */
@@ -55,9 +68,7 @@ class TSP
     {
         $this->graph[$from][$to] = $distance;
         $this->graph[$to][$from] = $distance;
-        $this->graph[$from][$from] = 0;
-        $this->graph[$to][$to] = 0;
-        $this->setComputed(false);
+        $this->reset();
     }
 
     /**
@@ -68,10 +79,8 @@ class TSP
         if ($this->isComputed()) {
             return;
         }
-
         $keys = array_keys($this->graph);
         $this->routes = $this->permutations($keys);
-
         foreach ($this->routes as $key => $perms) {
             $total = 0;
             for ($i = 0; $i < count($perms) - 1; $i++) {
@@ -86,8 +95,17 @@ class TSP
                 $this->shortestPaths = [];
             }
 
+            if ($total > $this->longestDistance || $this->longestDistance === null) {
+                $this->longestDistance = $total;
+                $this->longestPath = $perms;
+                $this->longestPaths = [];
+            }
+
             if ($total == $this->shortestDistance) {
                 $this->shortestPaths[] = $perms;
+            }
+            if ($total == $this->longestDistance) {
+                $this->longestPaths[] = $perms;
             }
         }
 
@@ -99,29 +117,30 @@ class TSP
      * @param array $perms
      * @return mixed
      */
-    private function permutations($items, $perms = [])
+    private function permutations($items): array
     {
-        static $permutations;
+        if (count($items) === 0) {
+            return [[]];
+        }
 
-        if (empty($items)) {
-            $permutations[] = $perms;
-        } else {
-            for ($i = count($items) - 1; $i >= 0; --$i) {
-                $newItems = $items;
-                $newPerms = $perms;
-                list($foo) = array_splice($newItems, $i, 1);
-                array_unshift($newPerms, $foo);
-                $this->permutations($newItems, $newPerms);
+        $firstItem = array_shift($items);
+        $permsWithoutFirst = $this->permutations($items);
+        $allPermutations = [];
+
+        foreach ($permsWithoutFirst as $perms){
+            for($i=0; $i<=count($perms); $i++){
+                $permsWithFirst = array_merge(array_slice($perms, 0, $i), [$firstItem], array_slice($perms, $i));
+                $allPermutations[] = $permsWithFirst;
             }
         }
 
-        return $permutations;
+        return $allPermutations;
     }
 
     /**
      * @return array
      */
-    public function getShortestPath()
+    public function getShortestPath(): array
     {
         $this->compute();
 
@@ -131,7 +150,7 @@ class TSP
     /**
      * @return array
      */
-    public function getShortestPaths()
+    public function getShortestPaths(): array
     {
         $this->compute();
 
@@ -139,9 +158,9 @@ class TSP
     }
 
     /**
-     * @return null
+     * @return int|null
      */
-    public function getShortestDistance()
+    public function getShortestDistance(): ?int
     {
         $this->compute();
 
@@ -151,7 +170,46 @@ class TSP
     /**
      * @return array
      */
-    public function getRoutes()
+    public function getLongestPath(): array
+    {
+        $this->compute();
+
+        return $this->longestPath;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLongestPaths(): array
+    {
+        $this->compute();
+
+        return $this->longestPaths;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getLongestDistance()
+    {
+        $this->compute();
+
+        return $this->longestDistance;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGraph(): array
+    {
+        return $this->graph;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getRoutes(): array
     {
         $this->compute();
 
@@ -175,5 +233,17 @@ class TSP
         $this->computed = $computed;
 
         return $this;
+    }
+
+    public function reset()
+    {
+        $this->computed = false;
+        $this->longestDistance = null;
+        $this->shortestDistance = null;
+        $this->longestPaths = [];
+        $this->longestPath = [];
+        $this->shortestPath = [];
+        $this->shortestPaths = [];
+        $this->routes = [];
     }
 }
