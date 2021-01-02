d37a258edabe05f235e48634ebb3a0d360a8dfc3
Vancoillie <jeremy.vancoillie@gmail.com>
2021-01-02 16:27:00 +0100

    Failled Year 2015 Day 24

---
+++ src/Puzzle/Year2015/Day24/PuzzleResolver.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Puzzle\Year2015\Day24;
+
+use App\Puzzle\AbstractPuzzleResolver;
+use App\Puzzle\PuzzleInput;
+use App\Utils\Generator;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class PuzzleResolver
+ * @see https://adventofcode.com/2015/day/24
+ *
+ * Not found @see https://www.reddit.com/r/adventofcode/comments/3y1s7f/day_24_solutions/
+ */
+class PuzzleResolver extends AbstractPuzzleResolver
+{
+    private $packages = [];
+
+    public function main(PuzzleInput $input, OutputInterface $output, $options = [])
+    {
+        $this->part1($input, $output);
+        $this->part2($input, $output);
+    }
+
+    public function part1(PuzzleInput $input, OutputInterface $output)
+    {
+        $packages = explode("\n", $input->getData());
+        $list = $this->createGroups(array_sum($packages)/3, $packages);
+        $min = INF;
+        foreach ($list as $item) {
+            $product = array_product($item);
+            if ($product < $min) {
+                $min = $product;
+            }
+        }
+
+        $output->writeln("<info>Part 1 : $min</info>");
+    }
+
+    public function part2(PuzzleInput $input, OutputInterface $output)
+    {
+        $packages = explode("\n", $input->getData());
+        $list = $this->createGroups(array_sum($packages)/4, $packages);
+        $min = INF;
+        foreach ($list as $item) {
+            $product = array_product($item);
+            if ($product < $min) {
+                $min = $product;
+            }
+        }
+
+        $output->writeln("<info>Part 1 : $min</info>");
+    }
+
+    private function createGroups($weight, $packages = [], $group = [], $checked = [])
+    {
+        $list = [];
+
+        while($packages){
+            $package = array_pop($packages);
+            $weightCheck = array_sum($group) + $package;
+            if($weight < $weightCheck){
+                $checked[] = $package;
+                continue;
+            }
+            $newGroup = $group;
+            $newGroup[] = $package;
+            if($weightCheck === $weight && $this->isValid(array_merge($checked, $packages), $weight)){
+                $list[] = $newGroup;
+            }
+
+            $list = array_merge($list, $this->createGroups($weight, $packages, $newGroup, $checked));
+        }
+        return array_unique($list,SORT_REGULAR);
+    }
+
+    private function isValid(array $group, $targetWeight, $weight = 0)
+    {
+        while($group) {
+            $item = array_pop($group);
+            $newWeight = $weight + $item;
+            if($newWeight === $targetWeight) {
+                return true;
+            }
+            if($newWeight < $targetWeight && $this->isValid($group, $targetWeight, $newWeight)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/Utils/Generator.php
+++ src/Utils/Generator.php
@@ -10,7 +10,7 @@ class Generator
     {
         if (count($dataset) === 0) {
             yield [];
-        }else{
+        } else {
             $key = array_key_first($dataset);
             $current = array_shift($dataset);
 
@@ -24,7 +24,7 @@ class Generator
         }
     }
 
-    public static function combinationsFixedSize(array $dataset, $size=1)
+    public static function combinationsFixedSize(array $dataset, $size = 1)
     {
         $originalLength = count($dataset);
         $remainingLength = $originalLength - $size + 1;
@@ -44,5 +44,35 @@ class Generator
                 }
             }
         }
+    }
+
+    public static function permutations($items)
+    {
+//        if (count($items) === 0) {
+//            yield [];
+//        }
+//
+//        $firstItem = array_shift($items);
+//        foreach (self::permutations($items) as $perms) {
+//            for ($i = 0; $i <= count($perms); $i++) {
+//                yield array_merge(array_slice($perms, 0, $i), [$firstItem], array_slice($perms, $i));
+//            }
+//        }
+
+        if (count($items) === 0) {
+            yield [];
+        }else{
+            $firstItem = array_shift($items);
+            $permsWithoutFirst = self::permutations($items);
+
+            foreach ($permsWithoutFirst as $perms){
+                for($i=0; $i<=count($perms); $i++){
+                    yield array_merge(array_slice($perms, 0, $i), [$firstItem], array_slice($perms, $i));
+                }
+            }
+        }
+
+
+
     }
 }
