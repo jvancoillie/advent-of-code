b658d444da6bc587d89ba07bf41b8aef90c1fd55
Vancoillie <jeremy.vancoillie@gmail.com>
2021-12-03 19:07:59 +0100

    Day 02

---
+++ src/Puzzle/Year2021/Day02/PuzzleResolver.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Puzzle\Year2021\Day02;
+
+use App\Puzzle\AbstractPuzzleResolver;
+use App\Puzzle\PuzzleInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class PuzzleResolver.
+ *
+ * @see https://adventofcode.com/2021/day/2
+ */
+class PuzzleResolver extends AbstractPuzzleResolver
+{
+    public function main(PuzzleInput $input, OutputInterface $output, $options = [])
+    {
+        $this->part1($input, $output);
+        $this->part2($input, $output);
+    }
+
+    public function part1(PuzzleInput $input, OutputInterface $output)
+    {
+        $ans = $this->navigate(explode("\n", $input->getData()));
+
+        $output->writeln("<info>Part 1 : $ans</info>");
+    }
+
+    public function part2(PuzzleInput $input, OutputInterface $output)
+    {
+        $ans = $this->navigateWithAim(explode("\n", $input->getData()));
+
+        $output->writeln("<info>Part 2 : $ans</info>");
+    }
+
+    private function navigate($data)
+    {
+        $depth = $horizontal = 0;
+        foreach ($data as $moves) {
+            [$action, $dist] = explode(' ', $moves);
+            switch ($action) {
+                case 'forward':
+                    $horizontal += (int) $dist;
+                    break;
+                case 'down':
+                    $depth += (int) $dist;
+                    break;
+                case 'up':
+                    $depth -= (int) $dist;
+                    break;
+            }
+        }
+
+        return $depth * $horizontal;
+    }
+
+    private function navigateWithAim($data)
+    {
+        $depth = $horizontal = $aim = 0;
+        foreach ($data as $moves) {
+            [$action, $dist] = explode(' ', $moves);
+            switch ($action) {
+                case 'forward':
+                    $horizontal += (int) $dist;
+                    $depth += $aim * (int) $dist;
+                    break;
+                case 'down':
+                    $aim += (int) $dist;
+                    break;
+                case 'up':
+                    $aim -= (int) $dist;
+                    break;
+            }
+        }
+
+        return $depth * $horizontal;
+    }
+}
