adc38e617040933733e77b1af9db43fd05fb16f1
Vancoillie <jeremy.vancoillie@gmail.com>
2021-12-05 07:37:13 +0100

    Day 05

---
+++ src/Puzzle/Year2021/Day05/PuzzleResolver.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Puzzle\Year2021\Day05;
+
+use App\Puzzle\AbstractPuzzleResolver;
+use App\Puzzle\PuzzleInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class PuzzleResolver.
+ *
+ * @see https://adventofcode.com/2021/day/5
+ */
+class PuzzleResolver extends AbstractPuzzleResolver
+{
+    /**
+     * @var Segment[]
+     */
+    private array $segments;
+
+    public function main(PuzzleInput $input, OutputInterface $output, $options = [])
+    {
+        $this->parseInput($input);
+
+        $this->part1($output);
+        $this->part2($output);
+    }
+
+    public function part1(OutputInterface $output)
+    {
+        $ans = $this->countOverlapping(false);
+
+        $output->writeln("<info>Part 1 : $ans</info>");
+    }
+
+    public function part2(OutputInterface $output)
+    {
+        $ans = $this->countOverlapping(true);
+
+        $output->writeln("<info>Part 2 : $ans</info>");
+    }
+
+    private function parseInput(PuzzleInput $input)
+    {
+        foreach (explode("\n", $input->getData()) as $entry) {
+            if (preg_match('/(?<x1>\d+),(?<y1>\d+) -> (?<x2>\d+),(?<y2>\d+)/', $entry, $matches)) {
+                $this->segments[] = new Segment($matches['x1'], $matches['y1'], $matches['x2'], $matches['y2']);
+            }
+        }
+    }
+
+    private function countOverlapping(bool $withDiagonals): int
+    {
+        $grid = [];
+
+        foreach ($this->segments as $segment) {
+            foreach ($segment->getPoints($withDiagonals) as [$x, $y]) {
+                if (isset($grid[$y][$x])) {
+                    ++$grid[$y][$x];
+                } else {
+                    $grid[$y][$x] = 1;
+                }
+            }
+        }
+
+        // count overlapping vents in grid, x,y > 1
+        return array_reduce($grid, function ($carry, $item) { return $carry + count(array_filter($item, function ($e) {return $e > 1; })); });
+    }
+}
---
+++ src/Puzzle/Year2021/Day05/Segment.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Puzzle\Year2021\Day05;
+
+class Segment
+{
+    private int $x1;
+    private int $y1;
+    private int $x2;
+    private int $y2;
+
+    public function __construct(int $x1, int $y1, int $x2, int $y2)
+    {
+        $this->x1 = $x1;
+        $this->y1 = $y1;
+        $this->x2 = $x2;
+        $this->y2 = $y2;
+    }
+
+    public function getPoints($withDiagonals = false): array
+    {
+        $points = [];
+        if ($this->x1 == $this->x2) {
+            foreach (range($this->y1, $this->y2) as $y) {
+                $points[] = [$this->x1, $y];
+            }
+        }
+
+        if ($this->y1 === $this->y2) {
+            foreach (range($this->x1, $this->x2) as $x) {
+                $points[] = [$x, $this->y1];
+            }
+        }
+
+        if ($withDiagonals && $this->x1 !== $this->x2 && $this->y1 !== $this->y2) {
+            $dy = ($this->y1 < $this->y2) ? 1 : -1;
+            $dx = ($this->x1 < $this->x2) ? 1 : -1;
+
+            $x = $this->x1;
+            $y = $this->y1;
+
+            while ($this->x2 !== $x && $this->y2 !== $y) {
+                $points[] = [$x, $y];
+                $x += $dx;
+                $y += $dy;
+            }
+
+            $points[] = [$this->x2, $this->y2];
+        }
+
+        return $points;
+    }
+}
