98db435d3a6f2bd1591673911e8e06ddfdbd3726
Vancoillie <jeremy.vancoillie@gmail.com>
2021-12-16 19:40:46 +0100

    use base_convert instead of hardcode mapping

--- src/Puzzle/Year2021/Day16/PuzzleResolver.php
+++ src/Puzzle/Year2021/Day16/PuzzleResolver.php
@@ -17,24 +17,6 @@ class PuzzleResolver extends AbstractPuzzleResolver
     protected static int|string $part1Expected = 852;
     protected static int|string $part2Expected = 19348959966392;
 
-    private array $map = [
-        '0' => '0000',
-        '1' => '0001',
-        '2' => '0010',
-        '3' => '0011',
-        '4' => '0100',
-        '5' => '0101',
-        '6' => '0110',
-        '7' => '0111',
-        '8' => '1000',
-        '9' => '1001',
-        'A' => '1010',
-        'B' => '1011',
-        'C' => '1100',
-        'D' => '1101',
-        'E' => '1110',
-        'F' => '1111',
-    ];
     private const OPERATOR = 'OPERATOR';
     private const LITERAL = 'LITERAL';
 
@@ -45,7 +27,7 @@ class PuzzleResolver extends AbstractPuzzleResolver
         $data = str_split($this->getInput()->getData());
         $bits = '';
         foreach ($data as $e) {
-            $bits .= $this->map[$e];
+            $bits .= sprintf('%04d', base_convert($e, 16, 2));
         }
 
         $this->result = $this->parse($bits);
