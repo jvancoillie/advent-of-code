99820e2c41e3b66d5196807097e6b531075c72b6
Vancoillie <jeremy.vancoillie@gmail.com>
2022-12-05 20:49:24 +0100

    refactor: day 05

--- src/Puzzle/Year2022/Day05/PuzzleResolver.php
+++ src/Puzzle/Year2022/Day05/PuzzleResolver.php
@@ -17,52 +17,49 @@ class PuzzleResolver extends AbstractPuzzleResolver
     protected static int|string $part1Expected = 'SBPQRSCDF';
     protected static int|string $part2Expected = 'RGLVRCQSB';
 
-    public function part1()
+    public function part1(): string
     {
-        [$entry, $moves] = $this->getParsedData();
+        [$stacks, $moves] = $this->getParsedData();
 
-        foreach ($moves as $l) {
-            preg_match('/move (.*) from (.*) to (.*)/', $l, $m);
-            $move = array_slice($entry[(int) $m[2]], -$m[1]);
-            $entry[(int) $m[2]] = array_slice($entry[(int) $m[2]], 0, count($entry[(int) $m[2]]) - $m[1]);
-            $entry[(int) $m[3]] = array_merge($entry[(int) $m[3]], array_reverse($move));
-        }
-
-        return implode(array_map(fn ($e) => array_pop($e), $entry));
+        return $this->applyMoves($stacks, $moves);
     }
 
-    public function part2()
+    public function part2(): string
     {
-        [$entry, $moves] = $this->getParsedData();
+        [$stacks, $moves] = $this->getParsedData();
 
-        foreach ($moves as $l) {
-            preg_match('/move (.*) from (.*) to (.*)/', $l, $m);
-            $move = array_slice($entry[(int) $m[2]], -$m[1]);
-            $entry[(int) $m[2]] = array_slice($entry[(int) $m[2]], 0, count($entry[(int) $m[2]]) - $m[1]);
-            $entry[(int) $m[3]] = array_merge($entry[(int) $m[3]], $move);
-        }
-
-        return implode(array_map(fn ($e) => array_pop($e), $entry));
+        return $this->applyMoves($stacks, $moves, false);
     }
 
     private function getParsedData(): array
     {
-        $data = explode("\n\n", $this->getInput()->getData());
-        $entry = [];
+        $data = explode("\n\n", $this->getInput()->getData(false));
+        $entry = array_fill(1, 9, []);
+
         foreach (explode("\n", $data[0]) as $line) {
             $split = array_map('trim', str_split($line, 4));
             foreach ($split as $i => $value) {
                 if (preg_match('/\[(.*)\]/', $value, $matches)) {
-                    if (!isset($entry[$i + 1])) {
-                        $entry[$i + 1] = [];
-                    }
-                    array_unshift($entry[$i + 1], $matches[1]);
+                    $entry[$i + 1][] = $matches[1];
                 }
             }
         }
 
-        ksort($entry);
+        return [array_map(fn ($e) => array_reverse($e), $entry), explode("\n", $data[1])];
+    }
 
-        return [$entry, explode("\n", $data[1])];
+    protected function applyMoves(array $stacks, array $moves, bool $reversed = true): string
+    {
+        foreach ($moves as $l) {
+            preg_match('/move (.*) from (.*) to (.*)/', $l, $m);
+            $move = array_slice($stacks[(int) $m[2]], -$m[1]);
+            if ($reversed) {
+                $move = array_reverse($move);
+            }
+            $stacks[(int) $m[2]] = array_slice($stacks[(int) $m[2]], 0, count($stacks[(int) $m[2]]) - $m[1]);
+            $stacks[(int) $m[3]] = array_merge($stacks[(int) $m[3]], $move);
+        }
+
+        return implode(array_map(fn ($e) => array_pop($e), $stacks));
     }
 }
